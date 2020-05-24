68c634cdf7eb56e3705a7bfcb45169c61c07669c
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-05-24 08:12:45 +0300

    commit

--- src/com/modernjava/java9improvements/MultiLineTextBlockExample.java
+++ src/com/modernjava/java9improvements/MultiLineTextBlockExample.java
@@ -1,36 +1,37 @@
 package com.modernjava.java9improvements;
 
 public class MultiLineTextBlockExample {
-    public static void main(String[] args) {
-        String st1 = """
-                    Hello World
-                    Using 
-                    text blocks !""";
+	public static void main(final String[] args) {
 
-        System.out.println("st1 = " + st1);
-        
-        String phrase = """
-                        { 
-                        employee : "Mike", 
-                        employeeId: 10001; 
-                        employeeType: FT
-                        }
-                        """;
-        System.out.println("phrase = " + phrase);
-        
-        String html = """
-                    <html>
-                        <body>
-                        <p>"Java, Programming"</p>
-                        </body>
-                    </html>
-                    """;
+		final String st1 = """
+				Hello World
+				Using
+				text blocks !""";
 
-        System.out.println("html = " + html);
-        
-        String st2 = st1.concat("This is my first text block");
-        System.out.println("st2 = " + st2);
+		System.out.println("st1 = " + st1);
 
+		final String phrase = """
+				{
+				employee : "Mike",
+				employeeId: 10001;
+				employeeType: FT
+				}
+				""";
 
-    }
+		System.out.println("phrase = " + phrase);
+
+		final String html = """
+				<html>
+				    <body>
+				    <p>"Java, Programming"</p>
+				    </body>
+				</html>
+				""";
+
+		System.out.println("html = " + html);
+
+		final String st2 = st1.concat("This is my first text block");
+		System.out.println("st2 = " + st2);
+
+	}
 }
